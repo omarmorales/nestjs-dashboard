3200c77b2f06d93218b6dc7026571b6f83f71a79
Omar Saul Morales Ibarra <saul090594@gmail.com>
2024-01-30 22:20:55 -0600

    adding lusitana font to correct deploy errors

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,7 @@
-import { Inter, Montserrat } from 'next/font/google';
+import { Inter, Montserrat, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
 export const montserrat = Montserrat({ weight: ['400', '700'], subsets: ['latin'] });
+
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
