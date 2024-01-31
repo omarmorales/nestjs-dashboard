21f28dc235905640805998c9da9b8e61e8ec328e
Omar Saul Morales Ibarra <saul090594@gmail.com>
2024-01-30 22:08:38 -0600

    learning nestjs

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter, Montserrat } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
-export const montserrat = Montserrat({ subsets: ['latin'] });
+export const montserrat = Montserrat({ weight: ['400', '700'], subsets: ['latin'] });
